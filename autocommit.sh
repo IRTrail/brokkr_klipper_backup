01d729989626da9316bea4c635aa559aac084565
Ivan Trail <irtrail@gmail.com>
2022-12-13 15:24:08 -0600

    Autocommit from 2022-12-13 15:24:08
    Klipper on commit: 69f76b3
    
    Moonraker on commit: dde9bcc
    
    Mainsail version: v2.4.1

--- autocommit.sh
+++ autocommit.sh
@@ -73,7 +73,7 @@ push_config(){
   git add .
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-  git push origin master
+  git push origin main
 }
 
 grab_version
